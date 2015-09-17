9028939c8062a5e502d2a81b99f0527d64fb3eb1
Julien CROUZET <jcrouzet.actemaconsulting@tf1.fr>
2015-09-17 23:47:59 +0200

    First version

---
+++ README.md
@@ -0,0 +1,71 @@
+akamai-time-reference
+-------
+
+# What is this ?
+
+`akamai-time-reference` is a Node.js package to get the exact current time using
+[Akamai's Time Reference service](https://developer.akamai.com/stuff/Akamai_Time_Reference/AkamaiTimeReference.html).
+
+It has only two depedencies :
+
+- [bluebird](https://github.com/petkaantonov/bluebird)
+- [request](https://github.com/request/request)
+
+# What it isn't
+
+- An [NTP](https://en.wikipedia.org/wiki/Network_Time_Protocol) client, see [ntp-client](https://www.npmjs.com/package/ntp-client)
+- More generally, a solution to synchronize your local clock
+- A banana
+
+# How it works ?
+
+```js
+const timeRef = require('akamai-time-reference');
+
+timeRef.now().then(function(now) {
+  console.log(now);
+});
+// >> Tue Sep 15 2015 21:36:53 GMT+0200 (CEST)
+
+timeRef.now().then(function(refNow) {
+  console.log('Local clock desync ins ms : %d', Date.now() - refNow);
+});
+// >> Local clock desync ins ms : 12765
+```
+
+The `now()` function can be passed an argument, which, if true, will skip the cache (to force a HTTP request).
+
+```js
+const timeRef = require('akamai-time-reference');
+
+timeRef.now();
+// >> HTTP request
+timeRef.now();
+// >> NO HTTP request, used cache
+timeRef.now(true);
+// >> HTTP request
+```
+
+# Options
+
+You can change some options like using `setOptions` by passing it an object with
+one/some of theses properties :
+
+```js
+timeRef.setOptions({
+  cacheTTL: 5000, // How often do we make a request to akamai, in ms (<=0 to disable)
+  useHTTP: false, // Use the HTTP protocol instead of HTTP**S**
+  timeout: 2000, // Request timeout value, in ms
+});
+
+// or just one :
+
+timeRef.setOptions({ cacheTTL: 1000 });
+
+```
+
+(Above example gives you the default values).
+
+Changing an/some options is done for every future calls (in the process), globally.
+
+`setOptions` will return the current options (You can pass an empty object to just get them).
---
+++ test/index.js
@@ -0,0 +1,148 @@
+'use strict';
+
+var chai = require('chai');
+var Promise = require('bluebird');
+var nock = require('nock');
+
+var expect = chai.expect;
+
+var timeRef = require('../index.js');
+
+chai.use(require('chai-as-promised'));
+
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+/* eslint-disable block-scoped-var */
+/* eslint-disable no-unused-expressions */
+describe('akamai-time-reference', function() {
+  this.timeout(0);
+  timeRef.setOptions({ timeout: 60000 });
+
+  describe('now()', function() {
+    it('should return a resolved Promise', function(done) {
+      var promise = timeRef.now();
+
+      expect(promise).to.be.an.instanceof(Promise);
+      promise.then(function() { done(); }, done);
+    });
+
+    it('should return a Promise of a valid date', function() {
+      var promise = timeRef.now();
+
+      return (Promise.all([
+        expect(promise).to.be.fulfilled,
+        expect(promise).to.be.fulfilled,
+        expect(promise).to.eventually.be.an.instanceof(Date),
+        // Should be above the time i'm writing this test right ?
+        promise.then(function(refDate) {
+          expect(refDate.getTime()).to.be.above(1442349364829);
+        }),
+      ]));
+    });
+
+    it('should return a rejected Promise if request fails/invalid', function() {
+      var fails = nock('https://time.akamai.com')
+        .get('/')
+        .reply(404, '404')
+        .get('/')
+        .reply(200, 'I am not a timestamp', {
+          Server: 'Akamai/Time Server',
+        })
+        .get('/')
+        .reply(200, '1442349364829', {
+          Server: 'Some other server header',
+        })
+      ;
+
+      return (Promise.all([
+        expect(timeRef.now(true)).to.be.rejected,
+        expect(timeRef.now(true)).to.be.rejected,
+        expect(timeRef.now(true)).to.be.rejected,
+      ]))
+      .then(function() {
+        if (!fails.isDone()) {
+          throw new Error('Mocks not done');
+        }
+      })
+      .finally(nock.restore)
+      .finally(nock.cleanAll);
+    });
+  });
+
+  describe('setOpt()', function() {
+    it('should accept valid properties', function() {
+      timeRef.setOptions({});
+      timeRef.setOptions({
+        cacheTTL: 5000,
+        useHTTP: false,
+        timeout: 60000,
+      });
+      expect(true).to.be.true;
+    });
+
+    it('should throws when passed invalid argument or invalid values', function() {
+      var thrower = function(param) {
+        return function() {
+          timeRef.setOptions(param);
+        };
+      };
+
+      expect(thrower()).to.throw(Error);
+      expect(thrower('test')).to.throw(Error);
+      expect(thrower({cacheTTL: 'test'})).to.throw(Error);
+      expect(thrower({timeout: {}})).to.throw(Error);
+    });
+  });
+
+  describe('options respect', function() {
+    it('cacheTTL', function(done) {
+      var mock = nock('https://time.akamai.com')
+        .get('/')
+        .reply(200, (new Date()).getTime(), {
+          Server: 'Akamai/Time Server',
+        })
+        .get('/')
+        .reply(200, (new Date()).getTime(), {
+          Server: 'Akamai/Time Server',
+        })
+      ;
+      var error;
+
+      timeRef.setOptions({cacheTTL: 60000});
+      return (
+        timeRef.now(true)
+          .then(function() { return (false); })
+          .then(timeRef.now)
+          .then(function() {
+            expect(mock.isDone()).to.be.false;
+            expect(mock.pendingMocks()).to.have.length(1);
+          })
+          .catch(function(err) { error = err; })
+          .finally(nock.restore)
+          .finally(nock.cleanAll)
+          .finally(function() { done(error); })
+      );
+    });
+
+    it('useHTTP', function(done) {
+      var mock = nock('http://time.akamai.com')
+        .get('/')
+        .reply(200, (new Date()).getTime(), {
+          Server: 'Akamai/Time Server',
+        });
+      var error;
+
+      timeRef.setOptions({timeout: 60000, useHTTP: true});
+      return (
+        (function() { return timeRef.now(true); }())
+          .then(function() {
+            expect(mock.done).to.not.throw;
+          })
+          .catch(function(err) { error = err; })
+          .finally(nock.restore)
+          .finally(nock.cleanAll)
+          .finally(function() { done(error); })
+      );
+    });
+  });
+});
